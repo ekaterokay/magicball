c678cdd72f8d7e736ff56e0b4e501a5ee8cbf214
Ekaterina <ekaterokay@gmail.com>
2026-02-23 21:40:40 +0300

    Добавлены изменения в структуре проекта

--- app/src/main/java/ui/AppRoot.kt
+++ app/src/main/java/ui/AppRoot.kt
@@ -13,6 +13,7 @@ import ui.screens.ScreenScaffold
 enum class Mode { CLASSIC, CUSTOM, DAILY }
 
 
+
 sealed class Screen {
     data object Home : Screen()
     data class ModeScreen(val mode: Mode) : Screen()
